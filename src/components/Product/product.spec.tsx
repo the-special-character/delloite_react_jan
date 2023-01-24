c5662d435f09cb069c3a3c13924a98106be5ab47
Yagnesh Modh <yagnesh.modh@gmail.com>
2023-01-24 10:31:40 +0530

    commit code for test product component

--- src/components/Product/product.spec.tsx
+++ src/components/Product/product.spec.tsx
@@ -56,3 +56,76 @@ describe('Product Component', () => {
     });
   });
 });
+
+describe('Product component with cartItem', () => {
+  const cartItem = { id: 1, productId: 1, quantity: 1 };
+
+  beforeEach(() => {
+    render(
+      <Product
+        product={products}
+        cartItem={cartItem}
+        addToCart={addToCart}
+        updateCartItem={updateCartItem}
+        deleteCartItem={deleteCartItem}
+        isAdding={false}
+        isUpdating={false}
+        isDeleting={false}
+      />,
+    );
+  });
+
+  it('should display edit and delete button', () => {
+    const btns = screen.queryAllByRole('button');
+    expect(btns.length).toBe(2);
+    expect(btns[0].innerHTML).toMatch('+');
+    expect(btns[1].innerHTML).toMatch('-');
+  });
+
+  it('should click edit button', () => {
+    const btns = screen.queryAllByRole('button');
+    expect(btns.length).toBe(2);
+    const editButton = btns[0];
+    fireEvent.click(editButton);
+    expect(updateCartItem).toBeCalledTimes(1);
+    expect(updateCartItem).toBeCalledWith({ ...cartItem, quantity: 2 });
+  });
+
+  it('should click delete button', () => {
+    const btns = screen.queryAllByRole('button');
+    expect(btns.length).toBe(2);
+    const deleteButton = btns[1];
+    fireEvent.click(deleteButton);
+    expect(deleteCartItem).toBeCalledTimes(1);
+    expect(deleteCartItem).toBeCalledWith(cartItem);
+  });
+});
+
+describe('Product component with more then 1 quantity cartItem', () => {
+  const cartItem = { id: 1, productId: 1, quantity: 2 };
+
+  beforeEach(() => {
+    render(
+      <Product
+        product={products}
+        cartItem={cartItem}
+        addToCart={addToCart}
+        updateCartItem={updateCartItem}
+        deleteCartItem={deleteCartItem}
+        isAdding={false}
+        isUpdating={false}
+        isDeleting={false}
+      />,
+    );
+  });
+
+  it('should click delete button and call edit function', () => {
+    const btns = screen.queryAllByRole('button');
+    expect(btns.length).toBe(2);
+    const deleteButton = btns[1];
+    fireEvent.click(deleteButton);
+    expect(deleteCartItem).toBeCalledTimes(0);
+    expect(updateCartItem).toBeCalledTimes(1);
+    expect(updateCartItem).toBeCalledWith({ ...cartItem, quantity: 1 });
+  });
+});
